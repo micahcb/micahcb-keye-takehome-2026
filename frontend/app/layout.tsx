5660dff6637f41f729720ee4b4782267f20de8d1
micahcb <micahcb@umich.edu>
2026-05-06 14:42:42 -0400

    add header to layout file to ensure consistency in the repo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist_Mono, IBM_Plex_Sans_Hebrew, Outfit, Poppins } from "next/font/goo
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/navbar";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -45,7 +46,10 @@ export default function RootLayout({
       )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
